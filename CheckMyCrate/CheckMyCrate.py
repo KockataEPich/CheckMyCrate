9a4be9a8d1994b2285fa45a882c4bb94659d7169
WoW Amaze So Amazing <kostadin.belchev@abv.bg>
2020-11-12 12:58:50 +0200

    Added the option to specify if the required resource is a file/string/directory with it outputing the appropriate feedback.

--- CheckMyCrate/CheckMyCrate.py
+++ CheckMyCrate/CheckMyCrate.py
@@ -18,7 +18,7 @@ import sys
 @click.group()
 @click.option('--verbose', is_flag=True)
 def program(verbose):
-    print('Welcome')
+    print('Welcome \n')
 
 @program.command()
 @click.argument('schema_instance', type=click.File('r'), required=True)
@@ -106,27 +106,64 @@ def check_my_crate(crate_path, profile_path):
    # Stop the program if the paths are no specified properly
    if isItViable:
    
-
+       # Constraint lists for feedback purposes
        must_constraint_list = []
        can_constraint_list = []
+
+       dir_must_missing_list = []
+       dir_can_missing_list = []
+
+       must_strings_missing_list = {}
+       can_strings_missing_list = {}
+
+       files_for_must_strings_missing_list = {}
+       files_for_can_strings_missing_list = {}
+
 
        keyword = ""
        path_file = ""
 
-       startOfConstraint = False;
-       startOfPath = False;
+       startOfConstraint = False
+       startOfPath = False
+       isStringConstraint = False
 
+       # Read constraints and check for them
        with open(profile_path) as f:
            for line in f:
                for c in line:
 
                    if c == "}":
+                       if check_dir(path_file,crate_path) == False:
+
+                           if keyword == "DMUST":
+                               dir_must_missing_list.append(path_file)
+
+                           if keyword == "DCAN":
+                               dir_can_missing_list.append(path_file)
+
                        if check_file(path_file,crate_path) == False:
-                           if keyword == "MUST":
+                           if keyword == "FMUST":
                                must_constraint_list.append(path_file)
 
-                           if keyword == "CAN":
+                           if keyword == "FCAN":
                                can_constraint_list.append(path_file)
+
+                       if keyword == "SCAN" or keyword == "SMUST":
+                           file_and_string = path_file.split(":")
+                                              
+                           if check_file(file_and_string[0], crate_path) == False:
+                               if keyword == "SMUST":
+                                   files_for_must_strings_missing_list[file_and_string[1]] = file_and_string[0] 
+                         
+                               if keyword == "SCAN":
+                                   files_for_can_strings_missing_list[file_and_string[1]] = file_and_string[0] 
+                           else:
+                               if check_string(file_and_string[0],crate_path, file_and_string[1]) == False:
+                                   if keyword == "SMUST":
+                                       must_strings_missing_list[file_and_string[1]] = file_and_string[0] 
+                         
+                                   if keyword == "SCAN":
+                                       can_strings_missing_list[file_and_string[1]] = file_and_string[0]
               
                        startOfConstraint = False;
                        startOfPath = False;
@@ -148,18 +185,79 @@ def check_my_crate(crate_path, profile_path):
 
        total_constraint_number_errors = len(must_constraint_list) + len(can_constraint_list)
 
-       click.echo("A total of number of constraints are not satisfied by this crate for this profile " +
+       click.echo("Total of number of file constraints are not satisfied by this crate for this profile " +
               str(total_constraint_number_errors))
 
-       click.echo("\n Must have files missing:")
+       click.echo("\n   Must have files missing:")
 
        for element in must_constraint_list:
-           click.echo("    " + element)
+           click.echo("      " + element)
 
-       click.echo("\n Can have files missing:")
+       click.echo("\n   Can have files missing:")
        for element in can_constraint_list:
-           click.echo("    " + element + "\n")
+           click.echo("      " + element + "\n")
+           
+       #############################################################
+       click.echo("\nTotal of number of directory constraints are not satisfied by this crate for this profile " +
+              str(len(dir_must_missing_list) + len(dir_can_missing_list)))
 
+       click.echo("\n   Must have directories missing:")
+
+       for element in dir_must_missing_list:
+           click.echo("      " + element)
+
+       click.echo("\n   Can have directories missing:")
+       for element in dir_can_missing_list:
+           click.echo("      " + element + "\n")
+
+       ############################################################
+       click.echo("\nTotal of number of string constraints are not satisfied by this crate because their file is missing:" +
+              str(len(files_for_must_strings_missing_list) + len(files_for_can_strings_missing_list)))
+
+       click.echo("\n")
+
+       for key,value in files_for_must_strings_missing_list.items():
+           click.echo("The MUST string:" + key + " is missing because the file:" + value + " could not be found" )
+
+          
+       click.echo("\n")
+       for key,value in files_for_can_strings_missing_list.items():
+           click.echo("The CAN string:" + key + " is missing because the file:" + value + " could not be found" )
+
+       ###################################################################################
+       click.echo("\nTotal of number of string constraints are not satisfied by this crate because they are missing from the specified files:" +
+           str(len(must_strings_missing_list) + len(can_strings_missing_list)))
+
+       click.echo("\n")
+
+       for key,value in must_strings_missing_list.items():
+           click.echo("The MUST string:" + key + " is missing int file:" + value)
+
+       click.echo("\n")
+       for key,value in can_strings_missing_list.items():
+           click.echo("The CAN string:" + key + " is missing in the file:" + value)
+
+
+@program.command()
+@click.argument('profile_path', required=True)
+def check_my_profile(crate_path, profile_path):
+   """This validates if the profile follows the appropriate syntax"""
+
+   if os.path.isfile(profile_path) == False:
+       click.echo("Profile file could not be found")
+       click.echo("Use --help for more information")
+   else:
+       profile_validate()
+
+
+def profile_validate():
+    OpenedBracket = False
+    
+   # with open(profile_path) as f:
+      #     for line in f:
+         #      for c in line:
+           #        if c != "{" and OpenedBracket == False
+                    
 
 def check_file(path_file, crate_path) -> bool:
     path_to_file = crate_path + path_file
@@ -169,6 +267,24 @@ def check_file(path_file, crate_path) -> bool:
     else:
        return False
 
+def check_dir(path_file, crate_path) -> bool:
+    path_to_file = crate_path + path_file
+
+    if os.path.isdir(path_to_file):
+       return True
+    else:
+       return False
+
+def check_string(path_file, crate_path, string_to_search) -> bool:
+   path_to_file = crate_path + path_file
+
+   with open(path_to_file, 'r') as read_obj:
+        # Read all lines in the file one by one
+        for line in read_obj:
+            # For each line, check if line contains the string
+            if string_to_search in line:
+                return True
+   return False
 
 if __name__ == '__main__':
     program()
